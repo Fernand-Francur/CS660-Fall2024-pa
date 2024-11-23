ffb93b5d455a80cb0375e8f02904d709989d4150
Timothy Borunov <timobohr@bu.edu>
2024-11-23 15:43:54 -0500

    finished pa4 without analytical question

--- src/db/ColumnStats.cpp
+++ src/db/ColumnStats.cpp
@@ -1,17 +1,87 @@
 #include <db/ColumnStats.hpp>
-
+#include <stdexcept>
 using namespace db;
 
-ColumnStats::ColumnStats(unsigned buckets, int min, int max)
-// TODO pa4: some code goes here
+ColumnStats::ColumnStats(unsigned buckets, int min, int max) : min(min), max(max), bucket_num(buckets)
 {
-  // TODO pa4: some code goes here
+  for(int i = 0; i < bucket_num; i++) {
+    bucket_counter.push_back(0);
+  }
+  int modulo = (max -  min)%(bucket_num);
+  divisor = (max -  min)/(bucket_num);
+  if (modulo) {
+    divisor++;
+  }
+  count = 0;
 }
 
 void ColumnStats::addValue(int v) {
-  // TODO pa4: some code goes here
+  if (v < min || max < v) {
+    throw std::runtime_error("value outside of hist range");
+  }
+  int index = (v - min) / divisor;
+  if (index == bucket_num) {index--;}
+  bucket_counter[index]++;
+  count++;
 }
-
+//enum class PredicateOp { EQ, NE, LT, LE, GT, GE };
 size_t ColumnStats::estimateCardinality(PredicateOp op, int v) const {
-  // TODO pa4: some code goes here
+  int normal_v = v - min;
+  int card = 0;
+  int index = normal_v / divisor;
+  int over = normal_v % (int )divisor;
+  if (index == bucket_num) {index--;}
+  int equality = (bucket_counter[index]/divisor);
+
+  if (index > bucket_num) {
+    equality = 0;
+  }
+
+  if (op == PredicateOp::EQ) {
+    return equality;
+  } else if (op == PredicateOp::NE) {
+    return count - equality;
+  } else if (op == PredicateOp::LT) {
+    if (index > bucket_num) {
+      return count;
+    }
+    card = over * bucket_counter[index] / divisor;
+
+    for (int i = 0; i < index; i++) {
+      card += bucket_counter[i];
+    }
+    return card;
+  } else if (op == PredicateOp::LE) {
+    if (index > bucket_num) {
+      return count;
+    }
+    card = (over+1) * bucket_counter[index] / divisor;
+
+    for (int i = 0; i < index; i++) {
+      card += bucket_counter[i];
+    }
+    return card;
+
+  } else if (op == PredicateOp::GT) {
+    if (index > bucket_num) {
+      return 0;
+    }
+    card = (divisor - 1 -over) * bucket_counter[index] / divisor;
+
+    for (int i = (bucket_num-1); i > index; i--) {
+      card += bucket_counter[i];
+    }
+    return card;
+
+  } else if (op == PredicateOp::GE) {
+    if (index > bucket_num) {
+      return 0;
+    }
+    card = (divisor -over) * bucket_counter[index] / divisor;
+
+    for (int i = (bucket_num-1); i > index; i--) {
+      card += bucket_counter[i];
+    }
+    return card;
+  }
 }
---
+++ src/db/writeup.md
@@ -0,0 +1,35 @@
+# Timothy Borunov PA4 Writeup Submission
+
+## (1) Design Decisions
+I simply followed the exact methodology provided. All functions perform as expected and there were no specific
+datastructures I really needed to specify or use. I tried to make my code as simple and fast as possible. The code,
+because I wrote it very quickly, might not be the most efficient but it gets the job done as that is not part of the
+assignment. I simply used a vector to have a data structure that I can index that has a variable size based on
+arguments provided, and a few spare integer values for math scratch space. I initially tried to optimize the code
+as best as I could in the manner of not reusing code segments, but due to the test cases being very explicit with
+how they wanted computation done, I had to match it and so could not optimize in the manner I thought I could due 
+to always having to round at the very end for accuracy sake. Otherwise, very simple code without any fancy 
+implementations.
+
+## (2) Missing or incomplete elements
+For the assignment, I fulfill all requirements and anything that can be considered missing is outside of the scope of
+the assignment. No elements are incomplete.
+
+## (3) Analytical questions
+No analytical questions are listed as analytical questions in the pa4 directory so I will assume this means technical
+questions and I will just copy my answers here as follows:
+
+### Question 1:
+
+### Question 2:
+
+### Question 3:
+
+## (4) Time and Challenges
+This was essentially a quick algorithm. The total code took me about 30 minutes. No challenges whatsoever because the
+assignment is very straightforward. Might be more challenging if there were more specific edgecases because I am sure
+I missed some that are very specific or user error based, but since we are dealing with more simplistic testcases, 
+there were no issues.
+
+## (5) Collaboration
+I worked completely on my own and only used stack overflow and cppreference.com for some syntax and library information.
